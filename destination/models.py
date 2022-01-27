75ffc020f6a0c2a2e9f3673f4d8090cdbec2577b
Manohar Dahal <manohardahal40@gmail.com>
2022-01-27 12:21:39 +0545

    my commit

--- destination/models.py
+++ destination/models.py
@@ -3,8 +3,6 @@ from django.db import models
 import datetime
 
 from django.utils import timezone
-
-# Create your models here.
 class rating(models.Model):
     rate=models.IntegerField(choices=[(1,1),(2,2),(3,3),(4,4),(5,5)]) 
    
@@ -13,13 +11,13 @@ class places(models.Model):
     name=models.CharField(max_length=30)
     address=models.CharField(max_length=30)
     descrption=models.TextField()
-    thumbnail_image=models.ImageField(upload_to="/images")
+    thumbnail_image=models.ImageField(upload_to="images")
 
 
 
 class destimages(models.Model):
     place=models.ForeignKey(places,on_delete=models.CASCADE)
-    image=models.ImageField(upload_to="/images")
+    image=models.ImageField(upload_to="images")
 
 
 class hotel(models.Model):
